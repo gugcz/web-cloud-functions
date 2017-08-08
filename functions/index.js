58c686be885ae5257f359bc0c8164b1814f7f511
horm <horak@horm.cz>
2017-08-08 20:41:14 +0200

    Get Organizers - Add query support for chapter

--- functions/index.js
+++ functions/index.js
@@ -7,48 +7,60 @@ admin.initializeApp(functions.config().firebase);
 // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloGDG = functions.https.onRequest((request, response) => {
     var gdgNamePromise = admin.database().ref('sections/gdg/name').once('value');
-console.log(gdgNamePromise)
 
-gdgNamePromise.then(nameSnapshot => response.send("Hello " + nameSnapshot.val() + " from Firebase!"));
+    gdgNamePromise.then(nameSnapshot => response.send("Hello " + nameSnapshot.val() + " from Firebase!"));
 });
 
 exports.getOrganizers = functions.https.onRequest((request, response) => {
-    var organizersPromise = admin.database().ref('organizers').once('value');
+    var organizersPromise;
+    let chapter = request.query.chapter;
+    let organizersReference = admin.database().ref('organizers');
 
-organizersPromise.then(organizerListSnapshot => sendOrganizerListWithName(organizerListSnapshot));
-
-
-function sendOrganizerListWithName(organizerListSnapshot) {
-    var organizers = organizerListSnapshot.val()
-
-    var organizersArray = Object.keys(organizers).map(function(k) { return organizers[k] });
-
-    var filteredAndShuffledOrganizersArray = shuffle(organizersArray.map(function filterOrganizerArray(organizer) {
-        return {name: organizer.name}
-    }))
-    response.send(filteredAndShuffledOrganizersArray)
-
-}
-
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    if (chapter) {
+        organizersPromise = organizersReference.orderByChild('chapters/'+ chapter).equalTo(true).once('value');
+    }
+    else {
+        organizersPromise = organizersReference.once('value');
     }
 
-    return array;
-}
 
+    organizersPromise.then(organizerListSnapshot => sendOrganizerListWithName(organizerListSnapshot));
+
+
+    function sendOrganizerListWithName(organizerListSnapshot) {
+        var organizers = organizerListSnapshot.val()
+
+        var organizersArray = Object.keys(organizers).map(function (k) {
+            return organizers[k]
+        });
+
+        // TODO - Remove inactive
+        var filteredAndShuffledOrganizersArray = shuffle(organizersArray.map(function filterOrganizerArray(organizer) {
+            return {name: organizer.name}
+        }))
+
+        response.send(filteredAndShuffledOrganizersArray)
+
+    }
+
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
 
 
 });
