029d890df0a69ef87b9bc60193b9d675f8912a4d
horm <horak@horm.cz>
2017-09-09 22:15:55 +0200

    Get Published Event - add chapters and organizers info

--- functions/event.js
+++ functions/event.js
@@ -133,10 +133,67 @@ exports.getPublishedEvent = function (request, response) {
     eventPromise.then(eventSnapshot => sendEventInfo(eventSnapshot));
 
     function sendEventInfo(chapterSnapshot) {
-        let event = chapterSnapshot.val();
+        let event = getFirsItemInKeyValue(chapterSnapshot.val());
 
-        response.send(event)
+
+        var organizersIds = Object.keys(event.organizers).map(function(key) {
+            if (event.organizers[key])
+                return key
+        })
+
+        var chaptersIds = Object.keys(event.chapters).map(function(key) {
+            if (event.chapters[key])
+                return key
+        })
+
+        Promise.all([getOrganizersInfo(organizersIds), getChaptersInfo(chaptersIds)]).then(result => {
+            event.organizers = result[0]
+            event.chapters = result[1]
+            console.log(event)
+            response.send(event)
+        })
     }
 }
 
+function getOrganizersInfo(organizersIds) {
+    var promises = organizersIds.map(function (id) {
+        return database.ref('organizers/' + id).once('value');
+    })
+
+    return Promise.all(promises).then(organizersInfo => {
+        console.log(organizersInfo[0].val())
+        return organizersInfo.map(organizer => {
+            return {
+                name: organizer.val().name,
+                profilePicture: ""
+            }
+        }
+        )
+    })
+}
+
+function getChaptersInfo(chaptersIds) {
+    var promises = chaptersIds.map(function (id) {
+        return database.ref('chapters/' + id).once('value');
+    })
+
+    return Promise.all(promises).then(chaptersInfo => {
+        console.log(chaptersInfo[0].val())
+        return chaptersInfo.map(chapter => {
+            return {
+                name: chapter.val().name,
+                key: chapter.key,
+                logo: ""
+            }
+        }
+        )
+    })
+}
+
+
+function getFirsItemInKeyValue(keyValue) {
+    return Object.keys(keyValue).map(function(key) {
+        return keyValue[key];
+    })[0]
+}
 
