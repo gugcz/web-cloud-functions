9f62adc550fe584f377d384b0ffbedb2d70cbe2f
HormCZ <horak@horm.cz>
2017-10-16 15:45:30 +0200

    Add push events to firestore function

--- functions/libs/database.js
+++ functions/libs/database.js
@@ -2,3 +2,4 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 exports.database = admin.database()
+exports.firestore = admin.firestore()
