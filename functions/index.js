801f1b54db6b3ea367abae38935cb7f5625c9530
HormCZ <horak@horm.cz>
2018-02-14 11:38:29 +0100

    Add auth for save event function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const database = require('./libs/database').database
+const admin = require('firebase-admin');
 
 
 /**
@@ -69,14 +70,42 @@ const adminEventModule = require('./admin/event')
 
 
 exports.saveEvent = functions.https.onRequest(((req, resp) => {
-
-  console.log(req.body.eventData)
-
-  console.log("body", req.body);
-  // TODO Add authentication and authorization
-  //adminEventModule.saveEvent(eventData).then(resp.send('Event saved'))
-  resp.send('OK')
+  validateFirebaseIdToken(req, resp, adminEventModule.saveEvent).then(resp.send('Event saved'));
 }))
+
+function validateFirebaseIdToken(req, res, next) {
+  console.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+    !req.cookies.__session) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>',
+      'or by passing a "__session" cookie.');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  }
+  return admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    return next(req, res);
+  }).catch((error) => {
+    console.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+  });
+};
+
 
 function publishedEventWasUpdated(eventSnapshot) {
   return eventSnapshot.val().published && eventSnapshot.val().publishedEventId;
