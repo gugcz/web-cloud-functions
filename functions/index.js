c30a4a7515dfdede99dcac2add2302b1663b2527
HormCZ <horak@horm.cz>
2018-12-08 11:48:13 +0100

    Add some basic stats

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,31 @@
 const functions = require('firebase-functions');
 
 const database = require('./libs/database').database
-/*exports.temporaryFunction = functions.https.onRequest(((req, resp) => {
-  database.ref('organizers').orderByChild('chapters_leader').startAt("").once('value').then(orgs => {
+exports.temporaryFunction = functions.https.onRequest(((req, resp) => {
+  const moment = require('moment');
+  const firebaseArray = require('./libs/firebase-array')
 
-    let promises = [];
-    orgs.forEach(org => {
-      console.log(org.val().chapters_leader)
-      promises.push(org.ref.child('chapters_leader').set({}))
-    })
+  const year = 2018;
+  const month = 1;
 
-    Promise.all(promises).then(() =>resp.send('ok')).catch(err => resp.send(err))
+  const startDate = moment([year, month - 1]);
+  const endDate = startDate.clone().add(12, 'month');
+
+  let eventPromise = database.ref('events')
+    .orderByChild('dates/start')
+    .startAt(startDate.toISOString())
+    .endAt(endDate.toISOString())
+    .once('value')
+
+  eventPromise.then(snapshot => {
+    let events = firebaseArray.getArrayFromKeyValue(snapshot.val())
+    const length = events.length;
+    const attendees = events.filter(event => event.report).reduce(((attendees, event) => attendees + event.report.attendeesCount), 0);
+    const organizers = [...new Set(events.map(events => firebaseArray.getArrayFromIdList(events.organizers)).reduce((result, orgArray) => result.concat(orgArray)))].length
+    //resp.send(organizers)
+    resp.send('Events: ' + length + '\nAttendees: ' + attendees + '\nOrganizers: ' + organizers);
   })
-}))*/
+}))
 
 /**
  * Frontend functions
